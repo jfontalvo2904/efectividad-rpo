a8eed2ccefaa745cdd14c3f60d91ac4eb6162aca
jfontalvo2904 <fontalvo0218@gmail.com>
2024-07-01 08:12:16 -0500

    update test

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/pages/login/login.component';
+import { NotFoundComponent } from './shared/pages/not-found/not-found.component';
 
 export const routes: Routes = [
     {
         path : 'login',
         component: LoginComponent
+    },
+    {
+        path : '**',
+        component: NotFoundComponent
     }
 ];
